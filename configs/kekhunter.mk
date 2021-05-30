c02834cfa5554f71b1cf82de5912e8ba583af45c
Ouroboros420 <42john42smith@googlemail.com>
2021-05-30 13:28:53 +0200

    Swap to latest official Nethunter apps and remove all the stuff dynamic devices don't need

--- configs/kekhunter.mk
+++ configs/kekhunter.mk
@@ -7,44 +7,6 @@ include vendor/kekhunter/firmware/wireless-usb/wireless-usb.mk
 
 # Apps
 PRODUCT_PACKAGES += \
-	kekhunter \
-	kekterm \
-	kex
-
-# KekStore stufff
-PRODUCT_PACKAGES += \
-	kekstore \
-	kekstore_priv
-
-# Hid side things
-PRODUCT_PACKAGES += \
-	init.nethunter.rc
-
-# Binaries NH
-PRODUCT_PACKAGES += \
-	bootkali \
-	bootkali_bash \
-	bootkali_env \
-	bootkali_init \
-	bootkali_log \
-	bootkali_login \
-	busybox_nh \
-	changemac \
-	chrootmgr \
-	duckyconventer \
-	hid-keyboard \
-	killkali \
-	macchanger \
-	setHID \
-	usbarmory \
-	usbtethering
-
-# Binaries 3party
-PRODUCT_PACKAGES += \
-	ifrename \
-	iwconfig \
-	iwevent \
-	iwgetid \
-	iwlist \
-	iwpriv \
-	iwspy
+	com.offsec.nethunter \
+	com.offsec.nhterm \
+	com.offsec.nethunter.store
